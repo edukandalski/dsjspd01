9dc2f15d88d84c51b77f6a286bc138650c6f97e1
Eduardo Kandalski <edukandalski@gmail.com>
2025-01-31 21:08:18 -0300

    Initial boilerplate

---
+++ src/main/java/com/eduardokandalski/dsjspd01/Dsjspd01Application.java
@@ -0,0 +1,13 @@
+package com.eduardokandalski.dsjspd01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsjspd01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsjspd01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardokandalski/dsjspd01/Dsjspd01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardokandalski.dsjspd01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsjspd01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
